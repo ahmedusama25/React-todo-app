2cc27ccf5d42d3284e51a3326ac3b469c69dd5f9
ahmedusama25 <ahmed.usama25@hotmail.com>
2020-10-31 12:18:31 +0500

    Added Delete Button

--- src/App.jsx
+++ src/App.jsx
@@ -15,21 +15,38 @@ class App extends Component{
     console.log(this.state.value) 
     this.state.value=""   
   }
+  delete=(index)=>{
+    // console.log(index)
+    this.state.todos.splice(index,1)
+    this.setState({todos:this.state.todos})
+  }
 
   render(){
     return(
       <div>
         <input value={this.state.value} onChange={(e)=>{this.setState({value:e.target.value})}} type="text" name="" id="" placeholder="Enter a value"/>
         <button onClick={this.Submit}>Submit</button>      
-        <ul>
-          {this.state.todos.map((v,i)=>{
-            return(
-            <li key={i}>{v}</li>
-            )
-          } )}
-        </ul>
-          
+        <table>
+          <caption>Todos List</caption>
 
+          
+            {this.state.todos.map((v,i)=>{
+
+              return(
+                <tbody key={i}><tr >
+                  <td>{i+1}</td>
+                  <td >{v}</td>
+                  <td><button onClick={this.Update}>update</button></td>
+                  <td><button onClick={()=> this.delete(i)}>Delete</button></td>
+                  
+                </tr>
+                </tbody>
+            
+              )
+            } )}
+          
+        </table>
+          
       </div>
 
 
@@ -40,3 +57,4 @@ class App extends Component{
 
 
 export default App;
+
